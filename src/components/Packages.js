3246762481a7cc4a3a1a7c7c07fe1fee0353ff62
Thomas Davidson <tomd1969@gmail.com>
2023-01-17 22:19:05 -0500

    added react-bootstrap functionality and formatting

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,15 @@
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
 
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage, i) => <ListGroup.Item action key={i}>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+        <Container>
+            <ListGroup>
+                {displayPackages}
+            </ListGroup>
+        </Container>
     )
 }
